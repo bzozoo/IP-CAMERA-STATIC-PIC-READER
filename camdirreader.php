ecbcf03c2865ed3a2ec441a22576756c5c12a31d
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-04-25 00:33:32 +0200

    Update camdirreader.php

--- camdirreader.php
+++ camdirreader.php
@@ -1,5 +1,4 @@
 <?php
-
 $dir = '/DATAS/CMS/CM1/'; // JPG upload folder of Camera (Change to your own folder)
 $globarry = glob($dir.'*.{jpg,JPG,jpeg,JPEG,png,PNG}',GLOB_BRACE);
 
@@ -10,11 +9,7 @@ $perpage = 100;
 $totalpage = ceil($countglobarry/$perpage);
 
 	 //Check pageGET exist or not
-	if (isset($page)) {
-         $pagenum = $page;
-         } else {
-	     $pagenum = 1;
-	     }
+	$pagenum = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 		
 	//Check Next
 	if ($pagenum == $totalpage) {
@@ -30,25 +25,17 @@ $totalpage = ceil($countglobarry/$perpage);
 	     $prev = ($pagenum - 1);
 	     }
 
-
 // Create a new Array for add pair file date and file url
-foreach($globarry as $file){
-	$imag =  $file;
-	$filedate = filemtime($imag);
-	$imgdataarray[] = array('picturl' => $imag, 'pictdate' => $filedate);
+foreach($globarry as $image){
+	$filedate = filemtime($image);
+	$imgdataarray[] = array('picturl' => $image, 'pictdate' => $filedate);
 }
 
-//Resort imgdataarray by date
-foreach ($imgdataarray as $key => $row) {
-    $pictdatekey[$key]  = $row['pictdate'];
-    $picturlkey[$key] = $row['picturl'];
-}
-
+//Sort imgdataarray by reverse date
 $pictdatekey  = array_column($imgdataarray, 'pictdate');
 $picturlkey = array_column($imgdataarray, 'picturl');
 
 array_multisort($pictdatekey, SORT_DESC, $picturlkey, SORT_ASC, $imgdataarray);
-
 
 // This code need to check Pagenumber is integer or not
 if ( preg_match('/^\d+$/', $pagenum) && ($pagenum > 0) && ($pagenum <= $totalpage))
@@ -87,13 +74,13 @@ foreach(array_slice($imgdataarray, $slicestart, $slicestop) as $key => $value) {
 
 			<?php
     } //Foreach end
-     
-	 } else { // If not an integer
-       echo "You must enter an integer as a page! It can't be display bigger pagenumber than total the pages! (We have only <a href='?page=$totalpage'>$totalpage</a> pages)<br /><hr/>";
-} // Integer check IF construct end 
-
 
 // Print out page datas In HTML              
 echo "<br /><div class='spacer' style='clear: both;'></div><hr /><div style='text-align: center; font-size: 20px;'>$countglobarry files  <br />";
 echo "<a href='?page=$prev'><< Prev </a> $pagenum / $totalpage . page(s) <a href='?page=$next'> Next >> </a> </div>";
+
+	 } else { // If not an integer
+       echo "You must enter an integer as a page! It can't be display bigger pagenumber than total the pages! (We have only <a href='?page=$totalpage'>$totalpage</a> pages)<br /><hr/>";
+} // Integer check IF construct end 
+
  ?>
