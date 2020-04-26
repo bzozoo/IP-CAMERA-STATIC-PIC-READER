d2153cfce366d292bf83941da8f5d49eca96bc34
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-04-26 17:05:20 +0200

    Update camreaddir.php
    Add session handler

--- camreaddir.php
+++ camreaddir.php
@@ -1,20 +1,34 @@
 <?php
-define("DIRECTORY", "/YOUR/CAMERA/STATIC/JPG-FILE/DIRECTORY"); // JPG upload folder of Camera (Change to your own folder)
-if (is_dir(DIRECTORY)) {
-	
- if (is_dir_empty(DIRECTORY)) {
-  echo "This directory is empty! We can display nothing."; 
-} else {
-    require_once('core.php'); 
-}
-	
+session_start();
+//user session check
+if(!empty($_SESSION["userId"])) {
+    require_once './view/userboard.php';
+	echo "Welcome $displayName! ";
+	echo "<a href='camlist.php' class='logout-button'>(CAMLIST)</a>";
+	echo "<a href='logout.php' class='logout-button'>(Logout)</a>";
+	echo "<br />";
+    define("DIRECTORY", "/YOUR/CAMERA/STATIC/JPG-FILE/DIRECTORY/"); // JPG upload folder of Camera (Change to your own folder)
+	     
+		 // Empty dir checking function
+	     function is_dir_empty($dir) {
+                  if (!is_readable(DIRECTORY)) return NULL; 
+                  return (count(scandir(DIRECTORY)) == 2);
+        }
+	   
+	   //Directory exist check
+       if (is_dir(DIRECTORY)) {
+		   //Directory empty check
+           if (is_dir_empty(DIRECTORY)) {
+               echo "This directory (".  DIRECTORY . ") is empty! We can display nothing."; 
+               } else {
+                       require_once('core.php'); 
+               }
  
-}else {
-  echo "This directory does not exist! Please define a new directory path!";
-}
+          } else {
+                   echo "This directory (".  DIRECTORY . ") does not exist! Please define a new directory path!";
+          }
 
-function is_dir_empty($dir) {
-  if (!is_readable(DIRECTORY)) return NULL; 
-  return (count(scandir(DIRECTORY)) == 2);
+    } else {
+            require_once './view/login-form.php';
 }
 ?>
