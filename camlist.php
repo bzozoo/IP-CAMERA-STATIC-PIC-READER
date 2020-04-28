b3ce38147e1b3b5965634085a721dc1a7ab8c0bc
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-04-28 23:31:08 +0200

    Add files via upload

--- camlist.php
+++ camlist.php
@@ -2,19 +2,34 @@
 session_start();
 if(!empty($_SESSION["userId"])) {
     require_once './view/userboard.php';
+	require_once './view/camboard.php';
+	$sessionUserID = $_SESSION['userId'];
 	echo "<html>";
 	echo "<head>";
 	echo "<title>CAMERA LIST</title>";
 	echo '<meta name="viewport" content="width=device-width, user-scalable=no">';
 	echo "</head>";
 	echo "<br />";
-    echo "Hello $displayName";
+		
+    echo "Hello $displayName UID($sessionUserID)";
+	echo "<br />";	echo "<br />";
+	
 	echo "<div style='font-size: 25px;'>";
-    echo "<a href='CM1.php'>LIST-CM1</a><br />";
-    echo "<a href='CM2.php'>LIST-CM2</a><br />";
-    echo "<a href='CM3.php'>LIST-CM3</a>";
-    echo "</div>";
+	//Camlist
+	
+	foreach($cameraListByUidArray as $key => $value1) {
+    echo ($key+1), ' - '; echo $value1['c_id'], ' - '; echo $value1['display_camname'], ' - '; echo $value1['cam_path'], ' - '; echo '<a href="CM-TEST.php?c_id=', $value1['c_id'], '">VIEW</a><br>';
+    }
+
+	echo "</div>";
 	echo "Click to <a href='logout.php' class='logout-button'>Logout</a>";
+	echo "<br />";
+	echo "<hr />";
+	echo "<pre>";
+    print_r($cameraListByUidArray);
+    echo "</pre>";
+	$key10 = array_search('4', array_column($cameraListByUidArray, 'c_id'));
+	echo $key10;
 	echo "<br />";
 	echo "</html>";
 } else {
