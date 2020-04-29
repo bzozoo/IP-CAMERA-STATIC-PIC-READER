e70258b169bb90f445203253b2d229c11ab5be46
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-04-30 00:10:27 +0200

    Added Install Camera Tables
    Instrall Tables for Camera: CamID, Name, Path, and Owner

--- sql/install.sql
+++ sql/install.sql
@@ -30,3 +30,39 @@ ALTER TABLE `registered_users`
 ALTER TABLE `registered_users`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
+
+
+
+
+
+--
+-- Table structure for table `registered_cameras
+--
+
+CREATE TABLE `registered_cameras` (
+  `c_id` int(8) NOT NULL,
+  `display_camname` varchar(255) NOT NULL,
+  `cam_path` varchar(255) NOT NULL,
+  `cam_owner` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `registered_cameras`
+--
+
+INSERT INTO `registered_cameras` (`c_id`, `display_camname`, `cam_path`, `cam_owner`) VALUES
+(1, 'DefaultCAM', '/DATAS/CMS/', 1); -- Change to Your own camera datas --
+
+--
+-- Indexes for table `registered_cameras
+--
+ALTER TABLE `registered_cameras`
+  ADD PRIMARY KEY (`c_id`);
+
+
+--
+-- AUTO_INCREMENT for table `registered_cameras
+--
+ALTER TABLE `registered_cameras`
+  MODIFY `c_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
