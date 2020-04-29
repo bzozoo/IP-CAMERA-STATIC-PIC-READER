55dc4d4c49bf6d9cda806378937f24b4fc8977d6
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-04-29 23:52:42 +0200

    New Camera reader
    It can change camera directory path with $_GET['cam_num']

--- camreader.php
+++ camreader.php
@@ -3,11 +3,18 @@ session_start();
 //user session check
 if(!empty($_SESSION["userId"])) {
     require_once './view/userboard.php';
-	echo "Welcome $displayName! ";
+	require_once './view/camboard.php';
+	
+	//CameraNumber
+    $actualCameraNumber = $_GET['cam_num'];
+    $actualCamPath = $cameraListByUidArray[($_GET['cam_num']-1)]['cam_path'];
+	
+	echo "Welcome $displayName on NUM-$actualCameraNumber camera!";
 	echo "<a href='camlist.php' class='logout-button'>(CAMLIST)</a>";
-	echo "<a href='logout.php' class='logout-button'>(Logout)</a>";
-	echo "<br />";
-    define("DIRECTORY", "/YOUR/CAMERA/STATIC/JPG-FILE/DIRECTORY/"); // JPG upload folder of Camera (Change to your own folder)
+	echo "<a href='logout.php' class='logout-button'>(Logout)</a><br />";
+	echo "Actual campath: $actualCamPath <br />";
+
+    define("DIRECTORY", $actualCamPath);
 	     
 		 // Empty dir checking function
 	     function is_dir_empty($dir) {
