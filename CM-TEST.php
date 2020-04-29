04fc274b5e546f410a8d0ba34cea297e1b787ef0
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-04-30 00:00:50 +0200

    New Camera reader test file
    New Camera reader test file

--- CM-TEST.php
+++ CM-TEST.php
@@ -4,11 +4,17 @@ session_start();
 if(!empty($_SESSION["userId"])) {
     require_once './view/userboard.php';
 	require_once './view/camboard.php';
-	echo "Welcome $displayName! ";
+	
+	//CameraNumber
+    $actualCameraNumber = $_GET['cam_num'];
+    $actualCamPath = $cameraListByUidArray[($_GET['cam_num']-1)]['cam_path'];
+	
+	echo "Welcome $displayName on NUM-$actualCameraNumber camera!";
 	echo "<a href='camlist.php' class='logout-button'>(CAMLIST)</a>";
-	echo "<a href='logout.php' class='logout-button'>(Logout)</a>";
-	echo "<br />";
-    define("DIRECTORY", "/DATAS/CMS/TEST/"); // JPG upload folder of Camera (Change to your own folder)
+	echo "<a href='logout.php' class='logout-button'>(Logout)</a><br />";
+	echo "Actual campath: $actualCamPath <br />";
+
+    define("DIRECTORY", $actualCamPath);
 	     
 		 // Empty dir checking function
 	     function is_dir_empty($dir) {
