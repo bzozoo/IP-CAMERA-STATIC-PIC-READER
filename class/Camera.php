3085c5790345abed9bda576c2451e2c0585e3f9e
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-03 14:13:34 +0200

    Add files via upload

--- class/Camera.php
+++ class/Camera.php
@@ -26,6 +26,16 @@ class Camera
         return $cameraResultByCID;
     }
 	
+	    function deleteCamera($RemoveCams)
+    {
+        $query = "DELETE FROM registered_cameras WHERE c_id = ? AND cam_owner = ? AND cam_secret = ?";
+        $paramType = "iii";
+        $paramArray = $RemoveCams;
+        $cameraDELResult = $this->ds->del($query, $paramType, $paramArray);
+        
+        return $cameraDELResult;
+    }
+	
 	    function getCameraByUserId($memberId)
     {
         $query = "select * FROM registered_cameras WHERE cam_owner = ?";
@@ -34,6 +44,16 @@ class Camera
         $cameraResultByUID = $this->ds->select($query, $paramType, $paramArray);
         
         return $cameraResultByUID;
+    }
+	
+		    function registerACamera($AddCameraDataArray)
+    {
+		$query = "INSERT INTO registered_cameras (display_camname, cam_path, cam_owner, cam_secret) VALUES (?, ?, ?, ?)";
+        $paramType = "ssii";
+        $paramArray = $AddCameraDataArray;
+        $insertCamId  = $this->ds->insert($query, $paramType, $paramArray);
+        
+        return $insertCamId;
     }
 
 }
