057e8f4cbed789105f8a28609198cbf4869c160f
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-03 14:34:33 +0200

    Add files via upload

--- view/userboard.php
+++ view/userboard.php
@@ -7,12 +7,20 @@ if (! empty($_SESSION["userId"])) {
     require_once __DIR__ . './../class/Member.php';
     $member = new Member();
     $memberResult = $member->getMemberById($_SESSION["userId"]);
+	$AllMemberResult = $member->getAllMember();
+	//Profile datas
     if(!empty($memberResult[0]["display_name"])) {
         $displayName = ucwords($memberResult[0]["display_name"]);
 		$uEmail = ucwords($memberResult[0]["email"]);
     } else {
         $displayName = $memberResult[0]["user_name"];
 		$uEmail = $memberResult[0]["email"];
+    }
+	// Admin check
+	    if(!empty($memberResult[0]["opt_adm"])) {
+        $adminCheck = 1;
+    } else {
+        $adminCheck = NULL;
     }
 }
 ?>
