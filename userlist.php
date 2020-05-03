52f9a60051d09a352b0bc29e9d16140c0e0e5631
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-03 15:12:29 +0200

    Userlist file for admin.
    In future Admin will can Add and Remove users here.

---
+++ userlist.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+	echo "<html>";
+	echo "<head>";
+	echo "<title>USEER LIST</title>";
+	echo '<meta name="viewport" content="width=device-width, user-scalable=no">';
+	echo "<link href='./view/css/style.css' rel='stylesheet' type='text/css' />";
+	echo "</head>";
+	echo "<br />";
+if(!empty($_SESSION["userId"])) {
+	
+    require_once './view/userboard.php';
+	if ($adminCheck != NULL) {
+	require_once 'functions.php';
+	$sessionUserID = $_SESSION['userId'];
+
+    echo "  <div class='dashboard'>
+            <div class='member-dashboard'>";
+    echo "Hello $displayName UID($sessionUserID) <a href='./'>[Dashboard]</a>";
+	echo "<br />";
+	
+	predumparray($AllMemberResult);
+	} else { echo "This page visible only for admin. Got to -> <a href='./'>Dashboard</a>"; }
+	echo "<br />";
+	echo "Click to <a href='logout.php' class='logout-button'>Logout</a>";
+	echo "<br />";	
+	echo "<hr />";
+	echo "  </div>
+            </div>";
+	echo "</html>";
+} else {
+    require_once './view/login-form.php';
+}
+?>
