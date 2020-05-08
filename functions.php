8d398bf1d4efce7e4b5bc84e6705224d0ad1f819
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-08 22:46:50 +0200

    Added dirisempty function

--- functions.php
+++ functions.php
@@ -8,4 +8,18 @@ function predumparray($dupedarr) {
 function simpledumparray($dupedarr) {
  print_r($dupedarr);    
 }
+
+// Empty dir checking function
+function dirisempty($dir) {
+  $handle = opendir($dir);
+  while (false !== ($entry = readdir($handle))) {
+    if ($entry != "." && $entry != "..") {
+      closedir($handle);
+      return FALSE;
+    }
+  }
+  closedir($handle);
+  return TRUE;
+}
+
 ?>
