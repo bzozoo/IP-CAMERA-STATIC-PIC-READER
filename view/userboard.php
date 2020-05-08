2ee4d8d7fd4f489153003766f0e81e3fa849fdc0
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-08 22:42:17 +0200

    Lot of changes

--- view/userboard.php
+++ view/userboard.php
@@ -6,16 +6,26 @@ use \Phppot\Member;
 if (! empty($_SESSION["userId"])) {
     require_once __DIR__ . './../class/Member.php';
     $member = new Member();
-    $memberResult = $member->getMemberById($_SESSION["userId"]);
+	
+	//Variable actual User ID
+	$sessionUserID = $_SESSION['userId'];
+    
+	// Member datas from ID
+	$memberResult = $member->getMemberById($_SESSION["userId"]);
+	
+	// All Member Array
 	$AllMemberResult = $member->getAllMember();
-	//Profile datas
+	
+	//Query Profile datas
     if(!empty($memberResult[0]["display_name"])) {
+		//Display name and email need in Camreader, Camlist views
         $displayName = ucwords($memberResult[0]["display_name"]);
 		$uEmail = ucwords($memberResult[0]["email"]);
     } else {
         $displayName = $memberResult[0]["user_name"];
 		$uEmail = $memberResult[0]["email"];
     }
+	
 	// Admin check
 	    if(!empty($memberResult[0]["opt_adm"])) {
         $adminCheck = 1;
