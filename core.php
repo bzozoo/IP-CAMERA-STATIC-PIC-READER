5deeb561ef0ea47e61bb0908a9f37a7e39d0975d
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-23 19:14:31 +0200

    Last modification
    Core.php will be delete

--- core.php
+++ core.php
@@ -9,90 +9,60 @@
 // Check If called directly or not
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 echo "This script is only for to include!"; exit;}
+?>
 
-$newCoreClass = new coreClass();
 
-// Glob files from defined dyrectory
-$ArrayedGlob = $newCoreClass->globArray(DIRECTORY);
-$imagARRAY = $newCoreClass->imagedataArray($ArrayedGlob);
-$countglobarry = count($ArrayedGlob);
-$negocountglobarry = (-1 * abs($countglobarry));
-$page = $_GET['page'];
-$perpage = 200;
-$totalpage = (int)ceil(count($ArrayedGlob) / $perpage);
+<!-- Head Links and etc... -->
+<a id='up'></a>
+<div style='text-align: center; font-size: 20px;'><?echo$countglobarry;?> files - <?echo$perpage;?> perpage <br />
+<!-- pagination1 -->
+<a href="?page=<?echo$prev;?>&cam_num=<?echo$actualCameraNumber;?>&sortdate=<?echo$sortByDate;?>">
+<button><< Prev </button>
+</a> 
 
-	 //Check pageGET exist or not
-	$pagenum = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-	$pagenum = max(1, min($totalpage, $pagenum));
-		
-	//Check Next
-	if ($pagenum == $totalpage) {
-         $next = 1;
-	     } else {
-	      $next = ($pagenum + 1); 
-	     }
-	
-	//Check Prev
-	if ($pagenum == 1) {
-         $prev = $totalpage;
-	     } else {
-	     $prev = ($pagenum - 1);
-	     }
-		 
-$slicestartcalculated = ($pagenum*$perpage)-$perpage; // Calculate Page start. Other: ($pagenum - 1) * $perpage;
-$slicestopcalculated = $negocountglobarry + ($pagenum * $perpage); // Calculate Page stop
-      
-  $slicestart = $slicestartcalculated; // Always equal
-           
-   if ($slicestopcalculated < 0) {
-       $slicestop = $slicestopcalculated; // Equal IF calculated  <0 
-	   } 
-	   else {
-      $slicestop = NULL; // Not calculated IF not <0
-      }
+<?echo$pagenum . " / " . $totalpage;?> . page(s) 
 
-// Head Links and etc...           
-echo "<a id='up'></a>
-<div style='text-align: center; font-size: 20px;'>$countglobarry files - $perpage perpage <br />";
-//pagination1
-echo "<a href='?page=$prev&cam_num=$actualCameraNumber&sortdate=$sortByDate'><< Prev </a> $pagenum / $totalpage . page(s) <a href='?page=$next&cam_num=$actualCameraNumber&sortdate=$sortByDate'> Next >> </a><br />";
-//pagination2
-	echo "<select id='pagenat' onchange='location = this.options[this.selectedIndex].value;'>";
+<a href='?page=<?echo$next;?>&cam_num=<?echo$actualCameraNumber;?>&sortdate=<?echo$sortByDate;?>'>
+<button> Next >> </button></a><br />
+<!-- pagination2 -->
+	<select id='pagenat' onchange='location = this.options[this.selectedIndex].value;'>
+<?php
 for ($pageNumber = 1;$pageNumber <= $totalpage;$pageNumber++):
      if($pageNumber == $page){
     echo "<option selected='selected' value='?page=$pageNumber&cam_num=$actualCameraNumber&sortdate=$sortByDate'>$pageNumber</option>";
 	 } else {
 	 echo "<option value='?page=$pageNumber&cam_num=$actualCameraNumber&sortdate=$sortByDate'>$pageNumber</option>"; }
 endfor;
-echo "</select>";
+?>
+</select>
 
-echo "<br /><a href='#down'>&darr;</a><br />
+<br /><a href='#down'>&darr;</a><br />
 </div><br />
 <hr />
 <div id='listcontent'>
 <div id='listcontentinner' style='display: table; margin: 0 auto;'>
-";
-			  
 
+			  
+<?php
 //List images from CoreClass
 $newCoreClass->foreachval($imagARRAY, $slicestart, $perpage); //Or perpage instead of slicestop
+?>
 
-// Page footer - Pagination links etc...            
-echo "</div><!-- /listcontentinner --></div><!-- /listcontent --><br />
+      <!-- Page footer - Pagination links etc... -->            
+      </div><!-- /listcontentinner --></div><!-- /listcontent --><br />
       <div class='spacer' style='clear: both;'></div>
       <hr />
       <div style='text-align: center; font-size: 20px;'>$countglobarry files <br />
       <a href='?page=$prev&cam_num=$actualCameraNumber&sortdate=$sortByDate#down'><< Prev </a> $pagenum / $totalpage . page(s) <a href='?page=$next&cam_num=$actualCameraNumber&sortdate=$sortByDate#down'> Next >> </a> <br />
-      <a id='down'></a> <br /><a href='#up'>&uarr;</a><br /></div>";
+      <a id='down'></a> <br /><a href='#up'>&uarr;</a><br /></div>
 
-// Modal Section
-echo "<!-- MODAL HTML SECTION -->
+      <!-- MODAL  SECTION -->
       <div id='myModal' class='modal'>
       <span class='close cursor' onclick='closeModal()'>&times;</span>
-      <div class='modal-content'>";
+      <div class='modal-content'>
 
+<?php
 //Modal Slider foreach
-
 foreach(array_slice($imagARRAY, $slicestart, $slicestop) as $key3 => $value3) {
 		$timestampkep3 = $value3['pictdate'];
 		$datekep3 = date("Y F d H:i:s", $timestampkep3);
@@ -107,15 +77,17 @@ foreach(array_slice($imagARRAY, $slicestart, $slicestop) as $key3 => $value3) {
                </div>
 	           <!-- MODAL Slider FOREACH HTML END -->";
 } //Modal Slider Foreach end
+?>
 
-// Modal slider buttons
-echo "<a class='prev' onclick='plusSlides(-1)'>&#10094;</a>
+       <!-- Modal slider buttons -->
+      <a class='prev' onclick='plusSlides(-1)'>&#10094;</a>
       <a class='next' onclick='plusSlides(1)'>&#10095;</a>
       <div class='caption-container'>
          <p id='caption'></p>
-      </div>";
+      </div>
 
-	//Modal thumb foreach
+<?php
+//Modal thumb foreach
 foreach(array_slice($imagARRAY, $slicestart, $slicestop) as $key2 => $value2) {
 		$timestampkep2 = $value2['pictdate'];
 		$datekep2 = date("Y F d H:i:s", $timestampkep2);
@@ -128,17 +100,15 @@ foreach(array_slice($imagARRAY, $slicestart, $slicestop) as $key2 => $value2) {
                </div>
 	           <!-- MODAL THUMB PIC FOREACH HTML END -->";
 } //Modal Thumb Foreach end 
+?>
 
-// Modal divs end
-echo "</div><!-- /modalcontent -->
-      </div><!-- /mymodal -->
-      <!-- MODAL HTML SECTION END -->";
+<!-- Modal divs end -->
+</div><!-- /modalcontent -->
+</div><!-- /mymodal -->
+<!-- MODAL HTML SECTION END -->
 
-// Include script for modal
-echo "<!-- Modal Script -->
-      <script src='./view/js/core-modal.js'></script>";
+<!-- Include Modal Script -->
+<script src='./view/js/core-modal.js'></script>
 
-// Close HTML tags
-echo "</body>
-      </html>";
- ?>
+    </body>
+</html>
