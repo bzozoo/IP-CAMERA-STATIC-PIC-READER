79b57bc74520dfc27f71cdf0b6adba79d60fa305
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-05-24 09:56:42 +0200

    We use popups.css now

--- camlist.php
+++ camlist.php
@@ -14,7 +14,7 @@ if(empty($_SESSION["userId"])) {
 <title>CAMERA LIST</title>
 <meta name="viewport" content="width=device-width, user-scalable=no">
 <link href='./view/css/style.css' rel='stylesheet' type='text/css' />
-<link href='./view/css/deletepopup.css' rel='stylesheet' type='text/css' />
+<link href='./view/css/popups.css' rel='stylesheet' type='text/css' />
 <link href='./view/css/camlist.css' rel='stylesheet' type='text/css' />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
